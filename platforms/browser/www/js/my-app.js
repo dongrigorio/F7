23e5ceaa45d7272f04701c64b77fdc2b1cc0505d
dongrigorio <dongrigorio@yandex.ru>
2017-01-11 14:56:55 +0500

    1
    2

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -60,30 +60,100 @@ var indexPage = myApp.onPageInit('index', function (page) {
         cBlock3.className = "card-content";
          
         var cBlock4 = document.createElement("div");
-        cBlock3.className = "card-content-inner";
+        cBlock4.className = "card-content-inner";
           
-        cBlock3.innerHTML = "<p>Цель = <b>" + prakticData.prakticLength + "</b></p>"
+        cBlock4.innerHTML = "<p>Цель = <b>" + prakticData.prakticLength + "</b></p>"
             + "<p>Выполнено = <b>" + prakticData.prakticSum + "</b></p>"
-            + "<p>% выполнения = <b>" + (prakticData.prakticSum/prakticData.prakticLength * 100 ^ 0) + "</b></p>"
+            //+ "<p>% выполнения = <b>" + (prakticData.prakticSum/prakticData.prakticLength * 100 ^ 0) + "</b></p>"
             + "<p><a href=\"praktic.html\" class=\"button button-big go-praktic\" type=\""
             + key
             + "\">Go!</a></p>";
         
+        //alert(cBlock4.innerHTML);
+        
+        var cBlock6 = document.createElement("a");
+        cBlock6.setAttribute("href", "praktic.html");
+        cBlock6.setAttribute("class", "go-praktic-2");
+        cBlock6.setAttribute("type", key);
+        
+        
+        var cBlock5 = document.createElement("div");
+        cBlock5.className = "ct-chart ct-double-octave";
+        cBlock5.innerHTML = "<style type=\"text/css\">"
+                        +   ".ct-series-a .ct-bar, .ct-series-a .ct-line, .ct-series-a .ct-point, .ct-series-a .ct-slice-donut {stroke: #39E639;}"
+                        +   ".ct-series-b .ct-bar, .ct-series-b .ct-line, .ct-series-b .ct-point, .ct-series-b .ct-slice-donut {stroke: #FF4040;}"
+                        +   "</style>";
+
+        //сделано в %
+        var piecesDone = +prakticData.prakticSum/+prakticData.prakticLength * 100 ^ 0;
+        
+        // осталось сделать в %
+        var picesDo;
+        
+        if (piecesDone >100) {
+            piecesDone =100;
+        }
+        // осталось в %
+        (+piecesDone >0) ? (piecesDo = 100 - +piecesDone):( piecesDo = 100);
+        /*
+        new Chartist.Pie(cBlock5, {
+          labels: ['Сделано', ''],
+          series: [+piecesDone, +piecesDo]
+        }, {
+            donut: true,
+            donutWidth: 30,
+            startAngle: 270,
+            total: 0,
+            showLabel: false,
+            horizontalBars: true,
+            color: "green",
+
+            chartPadding: 30,
+            //labelOffset: -100,
+            //labelDirection: 'explode'
+        });
+        
+        
+        */
+        new Chartist.Bar(cBlock5, {
+          labels: ['', ''],
+          series: [ [+piecesDone], [+piecesDo]]
+        }, {
+          stackBars: true,
+          horizontalBars: true,
+          chartPadding: 15,
+            seriesBarDistance: 100,
+            axisY: {
+                offset:10,
+                padding: 100,
+                position: 200
+            },
+            axisY: {
+               offset:10,
+                padding: 100,
+                position: 200
+            }
+        });
+        
+        
+        //cBlock6.appendChild(cBlock5);
+        cBlock4.appendChild(cBlock5); 
         cBlock3.appendChild(cBlock4);    
         cBlock1.appendChild(cBlock2);
         cBlock1.appendChild(cBlock3);
-         cBlock.appendChild(cBlock1);       
-    
+        cBlock.appendChild(cBlock1);       
+        
         var my_div = document.getElementById("page-content");  
         my_div.appendChild(cBlock);
-    
+        
     }
     
     //в параметер type лежит ключ к данным в ЛокалСторейдже
     $$('.go-praktic').on('click', function () {
-         prakticId = event.target.type;
-    });   
-    
+        prakticId = event.target.getAttribute("type"); // type;
+       
+    });  
+
 });
 
 
@@ -121,7 +191,7 @@ myApp.onPageInit('addPraktic', function (page) {
 });
 
 var pageInitPraktic = myApp.onPageInit('praktic', function (page) {
-    console.log(prakticId + " prakticId");
+    console.log("= "+prakticId + " = prakticId");
     var prakticData = JSON.parse(localStorage.getItem(prakticId));
     var date = new Date();
     var my_div = document.getElementById("content-block-title");  
@@ -129,34 +199,69 @@ var pageInitPraktic = myApp.onPageInit('praktic', function (page) {
     
     var options = {
      // era: 'long',
-      year: 'numeric',
+      year: '2-digit',
       month: 'short',
       day: 'numeric',
-      weekday: 'long',
+      //weekday: 'long',
       //timezone: 'UTC',
-      hour: 'numeric',
-      minute: 'numeric',
+      //hour: 'numeric',
+      //minute: 'numeric',
       //second: 'numeric'
     };
     piecePraktic=[]; 
     pieceDate=[];
-    var str = "<p>Установлена длина одного круга <b>" + prakticData["prakticCircleLength"] + "</b> </p>"
-                + "<p>Предыдущие сессии:  "; 
+    var str = "<p>Установлена длина одного круга <b>" + prakticData["prakticCircleLength"] + "</b> </p>";
     
-    if (prakticData.prakticPieces.length >0){
-    
-        var arr = prakticData.prakticPieces.split("=");
 
+    if (prakticData.prakticPieces.length > 0){
+        
+        var arr = prakticData.prakticPieces.split("=");
+        var labels1 = [], series1 = [];
+        var k;
+        k = 0;
         for (i=arr.length-1; i>=0; i--){
             var dataKey = arr[i].indexOf(":",0); //определили позицию разделителя
                 piecePraktic[i] = arr[i].substring(0,dataKey); //выделили кол-во повторений
                 pieceDate[i] = arr[i].substring(dataKey+1,arr[i].length); //выделили дату-время
                 date.setTime(pieceDate[i]);
-                str += "<br>" + date.toLocaleString("ru", options) +  " : " + piecePraktic[i] +" ";
+               // str += "<br>" + date.toLocaleString("ru-RU", options) +  " : " + piecePraktic[i] +" ";
+                labels1[i] = date.toLocaleString('en-GB', options);
+                series1[i] = +piecePraktic[i];
+                k += +piecePraktic[i];
         }
+        str += "<p>Среднее количеcтво повторений за одну сессию:  " + ((+k/(arr.length-1))^0);
+        str += "<p>Для достижения цели потребуется приблизительно " + ( (+prakticData.prakticLength - +prakticData.prakticSum)/(+k/(arr.length-1))^0  ) + " сессий.";
+        str += " </p>";        
+        
+        date.setTime(+pieceDate[arr.length-1]- +pieceDate[0] + 5*1000*60*60*24);
+        var periodDate = (date/ 24 / 60 / 60 / 1000 )^0;
+        
+        //for (i=periodDate; i>=0; i--){
+            
+           //if !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        
     }   
      
-    str += " </p>";
+    
+    
+    
+    
+    new Chartist.Bar('.ct-chart-day', {
+      labels: labels1,
+      series: [series1]
+    }, {
+      //seriesBarDistance: 10,
+      reverseData: true,
+      Width: 60,
+      horizontalBars: true,
+      axisY: {
+        offset: 70
+      }
+        
+    });
+    
+    
  
     my_div = document.getElementById("circle-length"); 
     my_div.innerHTML = str;
@@ -289,3 +394,34 @@ myApp.onPageInit('editPraktic', function (page) {
     
     
 });
+
+
+/*
+var data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10','W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [100, 200, 400, 800, 600, 20, 60, 55, 700, 2,100, 200, 400, 800, 600, 20, 60, 55, 700, 2]
+  ]
+};
+
+var options = {
+  //high: 10,
+  //low: -10,
+
+  axisX: {
+    showGrid: false,
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
+  },
+    width: "80%",
+  // Specify a fixed height for the chart as a string (i.e. '100px' or '50%')
+  height: "20%",
+  // Overriding the natural high of the chart allows you to zoom in or limit the charts highest displayed value
+};
+
+new Chartist.Bar('.ct-chart', data, options);
+
+
+
+*/
